32433f79351df8194f51dfe2952bda4f05cb1536
yegane <yeganehsalimi42@gmail.com>
2026-06-02 16:35:44 +0330

    adding the whole project

---
+++ src/main/java/com/library/dao/BookDAO.java
@@ -0,0 +1,120 @@
+package com.library.dao;
+
+import com.library.entity.Book;
+import com.library.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class BookDAO {
+
+    SessionFactory sessionFactory =
+            HibernateUtil.getSessionFactory();
+
+
+
+
+    public void save(Book book){
+
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        session.persist(book);
+
+        tx.commit();
+    }
+
+
+    public Book readById(int id){
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+
+        Book book = session.get(Book.class , id);
+
+        tx.commit();
+        return book;
+    }
+
+    public List<Book> readAllBooks(){
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+
+        Query<Book> bookQuery = session.createQuery("FROM Book ", Book.class);
+        List<Book> books = bookQuery.list();
+
+        tx.commit();
+        return books;
+
+    }
+
+    public List<Book> readByTitleContains(String text) {
+
+        if (text == null || text.trim().isEmpty()) {
+            throw new IllegalArgumentException("Search text cannot be empty");
+        }
+
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+
+
+        Query<Book> query = session.createQuery("FROM Book WHERE lower(title) LIKE :text", Book.class);
+
+        query.setParameter("text", "%" + text.toLowerCase() + "%");
+        List<Book> books = query.list();
+
+        tx.commit();
+
+        return books;
+
+
+    }
+
+
+    public void updateStatus(int id , boolean status){
+
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        Book book = session.get(Book.class , id);
+
+        book.setAvailable(status);
+
+        tx.commit();
+
+
+    }
+
+    public void updateStock(int id , int stock){
+
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        Book book = session.get(Book.class , id);
+
+        book.setBookStock(stock);
+
+
+
+        tx.commit();
+
+
+    }
+
+    public void delete(int id){
+
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        Book book = session.get(Book.class, id);
+        session.remove(book);
+
+        tx.commit();
+
+    }
+}
---
+++ src/main/java/com/library/dao/BorrowDAO.java
@@ -0,0 +1,102 @@
+package com.library.dao;
+
+
+
+import com.library.entity.Book;
+import com.library.entity.Borrow;
+import com.library.entity.Member;
+import com.library.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class BorrowDAO {
+    SessionFactory sessionFactory =
+            HibernateUtil.getSessionFactory();
+
+
+    public void insert(Member member , Book book ){
+
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Borrow borrow = new Borrow();
+        borrow.setBook(book);
+        borrow.setMember(member);
+        borrow.setBorrowDate(LocalDateTime.now());
+        session.persist(borrow);
+
+
+        tx.commit();
+
+    }
+
+
+    public List<Borrow> selectAll(){
+
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Query<Borrow> borrowQuery = session.createQuery("FROM Borrow ", Borrow.class);
+        List<Borrow> borrowList = borrowQuery.list();
+
+
+        tx.commit();
+
+        return borrowList;
+    }
+
+
+    public List<Borrow> selectMemberBook(Member member , Book book){
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+
+        Query<Borrow> borrowQuery =
+                session.createQuery(
+                        "FROM Borrow b WHERE b.member = :member AND b.book = :book",
+                        Borrow.class
+                );        borrowQuery.setParameter("member", member);
+        borrowQuery.setParameter("book", book);
+
+        List<Borrow> list = borrowQuery.list();
+        tx.commit();
+        return list;
+    }
+    public List<Borrow> findByMember(Member member ){
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+
+        try {
+            Query<Borrow> borrowQuery = session.createQuery("FROM Borrow b\n" +
+                    "JOIN FETCH b.book\n" +
+                    "WHERE b.member = :member", Borrow.class);
+
+            borrowQuery.setParameter("member", member);
+
+
+            List<Borrow> list = borrowQuery.list();
+            tx.commit();
+            return list;
+        } catch (Exception e) {
+            tx.rollback();
+            throw e;
+        }
+    }
+
+
+    public void returnBook(Integer borrowId){
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        Borrow borrow =  session.get(Borrow.class, borrowId);
+        borrow.setReturnDate(LocalDateTime.now());
+        tx.commit();
+
+
+    }
+
+
+}
